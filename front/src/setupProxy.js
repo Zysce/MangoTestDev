15ab02d1ca97a54ee1dda80afc421ab02b3837f2
Anthony <anth.lerouge@gmail.com>
2020-05-22 13:12:08 -0400

    Update setupProxy.js

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     "^/api/*",
     createProxyMiddleware({
-      target: "http://192.168.99.100:5000",
+      target: "http://localhost:5000",
       changeOrigin: true,
     })
   );
